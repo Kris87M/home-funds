4e3b2830f4162a7e9beb282bae97e6ba7b23bb3d
Paulina <gajewska.paulina08@gmail.com>
2025-02-24 20:39:34 +0100

    Fetch data in recurring-bills component

--- src/pages/RecurringBills/RecurringBills.js
+++ src/pages/RecurringBills/RecurringBills.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRecurringBills } from 'connector';
+import shortid from 'shortid';
 
 const RecurringBills = () => {
+  const dispatch = useDispatch();
+  const {items, status, error} = useSelector((state) => state.recurringBills);
+
+  useEffect(() => {
+    dispatch(fetchRecurringBills());
+  }, [dispatch]);
+
+  if(status === 'loading') return <p>Ładowanie...</p>;
+  if(status === 'failed') return <p>Błąd: {error}</p>;
+
   return (
     <div>
       <h1>Stałe wydatki</h1>
+      <ul>
+        {items.map(item => (
+          <li key={shortid}>{item.id} {item.name} {item.amount} {item.dueDate} {item.category}</li>
+        ))}
+      </ul>
+     
     </div>
   )
 }
