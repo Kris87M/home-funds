4a41b68497ed875f7d8ed7872fa85e20f4acfe4a
Paulina <gajewska.paulina08@gmail.com>
2025-02-25 19:50:08 +0100

    Add error notification

--- src/components/RecurringBills/RecurringBillssTable.js
+++ src/components/RecurringBills/RecurringBillssTable.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Table, Button, Spin } from "antd";
+import { Table, Spin, notification } from "antd";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchRecurringBills } from "connector";
 import columns from "./column";
@@ -11,9 +11,19 @@ const RecurringBillsTable = () => {
   useEffect(() => {
     dispatch(fetchRecurringBills());
   }, [dispatch]);
+  
+  useEffect(() => {
+    if (error) {
+      notification.error({
+        message: 'Błąd pobierania danych',
+        description: error,
+        placement: 'topRight',
+      });
+    }
+  }, [error]);
 
   if(status === 'loading') return <Spin size="large" style={{ display: "block", textAlign: "center", marginTop: 50 }} />
-  if(status === 'failed') return <p>Błąd: {error}</p>;
+  if(status === 'failed') return <div>Błąd: {error}</div>;
 
   return <Table columns={columns} dataSource={items}  rowKey="id" />;
 };
