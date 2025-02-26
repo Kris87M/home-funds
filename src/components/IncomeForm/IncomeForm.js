6781839d9664fde26cd28b18ebfb4fd51e4d9878
Paulina <gajewska.paulina08@gmail.com>
2025-02-26 19:59:09 +0100

    Add form structure to component

--- src/components/IncomeForm/IncomeForm.js
+++ src/components/IncomeForm/IncomeForm.js
@@ -1,5 +1,32 @@
+import { Formik } from "formik";
+
 const IncomeForm = () => {
-    return ;
+    return ( 
+        <Formik
+            initialValues={{ date: "", source: "", amount: "" }}
+            onSubmit={(values) => {
+                console.log("Dodano:", values);
+            }}
+        >
+            
+        {({ isSubmitting }) => (
+            <Form>
+                <label>Data:</label>
+                <Field type="date" name="date" />
+                <ErrorMessage name="date" component="div" style={{ color: "red" }} />
+
+                <label>Źródło dochodu:</label>
+                <Field type="text" name="source" />
+                <ErrorMessage name="email" component="div" style={{ color: "red" }} />
+
+                <label>Kwota:</label>
+                <Field type="number" name="amount" />
+                <ErrorMessage name="amount" component="div" style={{ color: "red" }} />
+
+                <button type="submit" disabled={isSubmitting}>Dodaj</button>
+            </Form>
+        )}
+        </Formik>);
 };
 
 export default IncomeForm;
