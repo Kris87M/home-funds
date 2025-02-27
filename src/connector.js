cfa24d80ce2634878f8c933cec6ec05001d77634
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-02-27 21:23:03 +0100

    Add deleteIncome function

--- src/connector.js
+++ src/connector.js
@@ -58,3 +58,17 @@ export const updateIncome = createAsyncThunk(
   }
 );
 
+export const deleteIncome = createAsyncThunk(
+  'income/deleteIncome',
+  async (id) => {
+    const response = await fetch(`http://localhost:4000/income/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (!response.ok) {
+      throw new Error('Nie udało się usunąć danych');
+    }
+
+    return id;
+  }
+);
