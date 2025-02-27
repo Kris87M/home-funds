c28e599ad87863355153da77c977ed254dc81581
Agata Szymeczko <agata.szymeczko21@gmail.com>
2025-02-27 17:46:16 +0100

    Code review fixes

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import TransactionsTable from 'components/Transactions/TransactionsTable'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchTransactions } from 'connector';
+import { Table, notification } from 'antd';
+import { columns } from './columns';
+import Spinner from 'components/Spinner/Spinner';
 
-const Transactions = () =>
-  <div>
-    <h1>Bieżące wydatki</h1>
+const Transactions = () => {
+  const dispatch = useDispatch();
+  const transactions = useSelector((state) => state.transactions.items);
+  const status = useSelector((state) => state.transactions.status);
+  const error = useSelector((state) => state.transactions.error);
 
-    <TransactionsTable />
-  </div>
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchTransactions());
+    }
+  }, [status, dispatch]);
 
+  useEffect(() => {
+    if (status === 'failed' && error) {
+      notification.error({
+        message: 'Błąd pobierania danych',
+        description: error,
+        placement: 'topRight',
+      });
+    }
+  }, [status, error]);
 
+  if (status === 'loading') {
+    return <Spinner />;
+  }
+
+  if (status === 'failed') {
+    return <p>Błąd: {error}</p>;
+  }
+
+  return (
+    <div>
+      <h1>Bieżące wydatki</h1>
+      <Table dataSource={transactions} columns={columns} rowKey="id" />
+    </div>
+  );
+}
 
 export default Transactions;
--- src/pages/Transactions/columns.js
+++ src/pages/Transactions/columns.js
@@ -3,28 +3,25 @@ export const columns = [
         title: 'Data',
         dataIndex: 'date',
         key: 'date',
-
-
-
         width: "20%",
-        sorter: true,
+        sorter: (a, b) => new Date(a.date) - new Date(b.date),
     },
     {
         title: 'Opis',
         dataIndex: 'description',
         key: 'description',
-        sorter: true,
+        sorter: (a, b) => a.description.localeCompare(b.description),
     },
     {
         title: 'Kwota',
         dataIndex: 'amount',
         key: 'amount',
-        sorter: true,
+        sorter: (a, b) => a.amount - b.amount,
     },
     {
         title: 'Kategoria',
         dataIndex: 'category',
         key: 'category',
-        sorter: true,
+        sorter: (a, b) => a.category.localeCompare(b.category),
     },
 ];
