13af594b9148409b97e19a31d432f869489448bd
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-02 18:41:02 +0100

    Move API call functions to apiConfig.js

--- src/connector.js
+++ src/connector.js
@@ -1,38 +1,31 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchData, postData, putData, deleteData } from 'config/apiConfig'
 
 export const fetchTransactions = createAsyncThunk(
   'transactions/fetchTransactions',
   async () => {
-    const response = await fetch('/transactions');
-    const data = await response.json();
-    return data;
+    return await fetchData('/transactions');
   }
 );
 
 export const fetchBalanceSheet = createAsyncThunk(
   'balanceSheet/fetchBalanceSheet',
   async () => {
-    const response = await fetch('/balance-sheet');
-    const data = await response.json();
-    return data;
+    return await fetchData('/balance-sheet');
   }
 );
 
 export const fetchIncome = createAsyncThunk(
   'income/fetchIncome',
   async () => {
-    const response = await fetch('/income');
-    const data = await response.json();
-    return data;
+    return await fetchData('/income');
   }
 );
 
 export const fetchRecurringBills = createAsyncThunk(
   'recurringBills/fetchRecurringBills',
   async () => {
-    const response = await fetch('/recurring-bills');
-    const data = await response.json();
-    return data;
+    return await fetchData('/recurring-bills');
   }
 );
 
@@ -40,34 +33,14 @@ export const updateIncome = createAsyncThunk(
   'income/updateIncome',
   async (updatedRecord) => {
     const { id, ...updatedData } = updatedRecord;
-
-    const response = await fetch(`http://localhost:4000/income/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify(updatedData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error('Nie udało się zaktualizować danych');
-    }
-
-    const data = await response.json();
-    return data;
+    return await putData(`/income/${id}`, updatedData);
   }
 );
 
 export const deleteIncome = createAsyncThunk(
   'income/deleteIncome',
   async (id) => {
-    const response = await fetch(`http://localhost:4000/income/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (!response.ok) {
-      throw new Error('Nie udało się usunąć danych');
-    }
+    await deleteData(`/income/${id}`);
     return id;
   }
 );
