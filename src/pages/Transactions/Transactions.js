37942fd7dc22bb503d29fbdae3a3dff1b56ecc73
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-07 23:12:46 +0100

    Add button to add new transaction in table

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getTransactions, updateTransactions, deleteTransactions } from 'connector';
-import { Table, notification, Form } from 'antd';
+import { getTransactions, addTransactions, updateTransactions, deleteTransactions } from 'connector';
+import { Table, notification, Form, Button } from 'antd';
 import { columns } from './columns';
 import Spinner from 'components/Spinner/Spinner';
 import SearchForm from 'components/SearchForm/SearchForm';
 import EditableModal from 'components/Modals/EditableModal';
 import dayjs from 'dayjs';
+import AddModal from 'components/Modals/AddModal';
 
 const Transactions = () => {
   const dispatch = useDispatch();
@@ -15,8 +16,10 @@ const Transactions = () => {
   const error = useSelector((state) => state.transactions.error);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [currentRecord, setCurrentRecord] = useState(null);
   const [form] = Form.useForm();
+  const [addForm] = Form.useForm();
   const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
@@ -72,6 +75,18 @@ const Transactions = () => {
     }
   };
 
+  const handleAddTransaction = async () => {
+    try {
+      const newTransaction = await addForm.validateFields();
+      newTransaction.date = newTransaction.date.format('YYYY-MM-DD');
+      dispatch(addTransactions(newTransaction));
+      setIsAddModalOpen(false);
+      addForm.resetFields();
+    } catch (error) {
+      console.error('Błąd walidacji formularza:', error);
+    }
+  };
+
   if (status === 'loading') {
     return <Spinner />;
   }
@@ -83,7 +98,16 @@ const Transactions = () => {
   return (
     <div>
       <h1>Bieżące wydatki</h1>
-      <SearchForm onSearch={setSearchValue} style={{ marginBottom: 8 }} />
+      <div style={{display: 'flex', gap: 8, marginBottom: 8}}>
+        <SearchForm onSearch={setSearchValue} />
+        <Button
+          type='primary'
+          onClick={() => setIsAddModalOpen(true)}
+          style={{ height: 40 }}
+        >
+          Dodaj
+        </Button>
+      </div>
       <Table dataSource={filteredTransactions} columns={columns(handleEdit, handleDelete)} rowKey="id" />
       <EditableModal
         isOpen={isModalOpen}
@@ -97,6 +121,21 @@ const Transactions = () => {
           { name: 'amount', label: 'Kwota', type: 'number', rules: [{ required: true, message: 'Wprowadź kwotę!' }] },
           { name: 'category', label: 'Kategoria', rules: [{ required: true, message: 'Wprowadź kategorię!' }] },
         ]}
+      />
+      <AddModal
+        isOpen={isAddModalOpen}
+        onCancel={() => setIsAddModalOpen(false)}
+        onSave={handleAddTransaction}
+        form={addForm}
+        title={"Dodaj kolejną transakcję"}
+        fields={
+          [
+            { name: 'date', label: 'Data', type: 'date', rules: [{ required: true, message: 'Wybierz datę!' }] },
+            { name: 'amount', label: 'Kwota', type: 'number', rules: [{ required: true, message: 'Podaj kwotę!' }] },
+            { name: 'description', label: 'Opis', type: 'text', rules: [{ required: true, message: 'Dodaj opis transakcji!' }] },
+            { name: 'category', label: 'Kategoria', type: 'text', rules: [{ required: true, message: 'Dodaj kategorię!'}]}
+          ]
+        }
       />
     </div>
   );
