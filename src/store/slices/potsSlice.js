ed57026f1aaeb152c27cabac6c9a2dd1af03bfbb
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-04-05 23:04:38 +0200

    Add addPots function

--- src/store/slices/potsSlice.js
+++ src/store/slices/potsSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getPots, updatePot } from 'connector';
+import { addPots, getPots, updatePot } from 'connector';
 
 const pots = createSlice({
   name: 'pots',
@@ -22,17 +22,17 @@ const pots = createSlice({
         state.status = 'failed';
         state.error = action.error.message;
       })
-      // .addCase(addPots.pending, (state) => {
-      //   state.status = 'loading';
-      // })
-      // .addCase(addPots.fulfilled, (state, action) => {
-      //   state.status = 'succeeded';
-      //   state.items.push(action.payload);
-      // })
-      // .addCase(addPots.rejected, (state, action) => {
-      //   state.status = 'failed';
-      //   state.error = action.error.message;
-      // })
+      .addCase(addPots.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(addPots.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.items.push(action.payload);
+      })
+      .addCase(addPots.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
       .addCase(updatePot.fulfilled, (state, action) => {
         state.items = state.items.map((item) =>
           item.id === action.payload.id ? action.payload : item
