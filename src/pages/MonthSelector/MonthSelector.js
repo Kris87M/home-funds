7daccded018ccd6e95680ea9e2c1351f71e35dd9
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-04-13 23:22:20 +0200

    Add monthly income/spending summary and handleMonthSelect

--- src/pages/MonthSelector/MonthSelector.js
+++ src/pages/MonthSelector/MonthSelector.js
@@ -1,32 +1,74 @@
 import Card from 'antd/es/card/Card'
-// import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { Button, Col, Row } from 'antd';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setSelectedMonth } from 'store/slices/monthSlice';
 
 const MonthSelector = () => {
-  // const totalIncome = useSelector(state => state.income.items.reduce((sum, item) => sum + item.amount, 0));
-  // const totalTransactions = useSelector(state => state.transactions.items.reduce((sum, item) => sum + item.amount, 0));
-  // const totalRecurringBills = useSelector(state => state.recurringBills.items.reduce((sum, item) => sum + item.amount, 0))
-  // const totalSpendings = totalTransactions + totalRecurringBills;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const incomeItems = useSelector(state => state.income.items);
+  const transactionsItems = useSelector(state => state.transactions.items);
+  const recurringBillsItems = useSelector(state => state.recurringBills.items);
 
   const months = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień']
+
+  const getMonthString = (index) => {
+    const year = 2025;
+    return `${year}-${String(index + 1).padStart(2, '0')}`;
+  };
+
+  const getMonthlySummary = (monthString) => {
+    const income = incomeItems
+      .filter(item => item.date.startsWith(monthString))
+      .reduce((sum, item) => sum + item.amount, 0);
+
+    const transactions = transactionsItems
+      .filter(item => item.date.startsWith(monthString))
+      .reduce((sum, item) => sum + item.amount, 0);
+
+    const bills = recurringBillsItems
+      .filter(item => item.dueDate?.startsWith(monthString))
+      .reduce((sum, item) => sum + item.amount, 0);
+
+    return {
+      income,
+      spending: transactions + bills,
+    };
+  };
+
+  const handleMonthSelect = (index) => {
+    const year = 2025;
+    const monthString = `${year}-${String(index + 1).padStart(2, '0')}`;
+    dispatch(setSelectedMonth(monthString));
+    navigate('/overview')
+};
 
   return (
     <div>
       <h1>Month Selector</h1>
       <Row gutter={16} >
-        {months.map((month) => (
-          <Col xs={24} sm={12} md={8} key={`pot-${month}`} style={{ marginBottom: 16 }}>
-            <Card title={month.toUpperCase()} variant="borderless" style={{ width: 300 }} extra={<Button type="primary">Szczegóły</Button>}>
-              {/* <p>Przychody: {totalIncome}</p>
-              <p>Wydatki: {totalSpendings}</p> */}
-      </Card>
+        {months.map((month, index) => {
+          const monthString = getMonthString(index);
+          const { income, spending } = getMonthlySummary(monthString);
+
+          return (
+            <Col xs={24} sm={12} md={8} key={`pot-${month}`} style={{ marginBottom: 16 }}>
+              <Card
+                title={month.toUpperCase()}
+                variant="borderless"
+                style={{ width: 300 }}
+                extra={<Button type="primary" onClick={() => handleMonthSelect(index)}>Szczegóły</Button>}
+              >
+                <p>Przychody: {income}</p>
+                <p>Wydatki: {spending}</p>
+              </Card>
             </Col>
-          ))}
-        </Row>
-
-
-
-
+          )
+        })}
+      </Row>
     </div>
   )
 }
