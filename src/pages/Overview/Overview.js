9fa3ead704db35f8d408c7001b75d9b98a3a7fb2
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-04-22 22:06:36 +0200

    Fix amount display

--- src/pages/Overview/Overview.js
+++ src/pages/Overview/Overview.js
@@ -1,8 +1,9 @@
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 import { Row } from 'antd'
-import OverviewCard from 'components/Cards/OverviewCard';
 import { useFilteredData } from 'hooks/useFiltredData';
+import { formatToTwoDecimalPlaces } from 'utils/formatToTwoDecimalPlacesUtils';
+import OverviewCard from 'components/Cards/OverviewCard';
 
 const calculateTotal = (items = []) => items.reduce((sum, item) => sum + item.amount, 0);
 
@@ -22,11 +23,11 @@ const Overview = () => {
     <div>
       <h1>Przegląd</h1>
       <Row gutter={16}>
-        <OverviewCard title='Przychody' total={totalIncome} type='income' onNavigate={() => navigate('/income')}/>
-        <OverviewCard title='Stałe wydatki' total={totalRecurringBills} type='recurringBills' onNavigate={() => navigate('/recurring-bills')}/>
-        <OverviewCard title='Bieżące wydatki' total={totalTransactions} type='transactions' onNavigate={() => navigate('/transactions')}/>
-        <OverviewCard title='Bilans' total={netWorth} type='netWorth' onNavigate={() => navigate('/balance-sheet')} />
-        <OverviewCard title='Skarbonki' total={totalSavedInPots} type='pots' onNavigate={() => navigate('/pots')} />
+        <OverviewCard title='Przychody' total={formatToTwoDecimalPlaces(totalIncome)} type='income' onNavigate={() => navigate('/income')}/>
+        <OverviewCard title='Stałe wydatki' total={formatToTwoDecimalPlaces(totalRecurringBills)} type='recurringBills' onNavigate={() => navigate('/recurring-bills')}/>
+        <OverviewCard title='Bieżące wydatki' total={formatToTwoDecimalPlaces(totalTransactions)} type='transactions' onNavigate={() => navigate('/transactions')}/>
+        <OverviewCard title='Bilans' total={formatToTwoDecimalPlaces(netWorth)} type='netWorth' onNavigate={() => navigate('/balance-sheet')} />
+        <OverviewCard title='Skarbonki' total={formatToTwoDecimalPlaces(totalSavedInPots)} type='pots' onNavigate={() => navigate('/pots')} />
       </Row> 
     </div>
   )
